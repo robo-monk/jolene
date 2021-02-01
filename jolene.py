aba1b1a063d56e5d8e5995ee0752217e4c1cccb5
robo-monk <rrobomonk@gmail.com>
2021-02-01 02:27:05 +0200

    small imporvement

--- jolene.py
+++ jolene.py
@@ -6,7 +6,7 @@ import sys
 import json
 
 _version = "0.1.0"
-_creds = "jolene/creds.json"
+_creds = ".jolene/creds.json"
 
 def _key():
     key = os.environ.get("JOLENE")
@@ -37,30 +37,16 @@ def encrypt_dic(dic):
 
     return enc
 
+def write(dic):
+    print('writing to file', dic)
+    with open(_creds, "w") as write_file:
+        json.dump(dic, write_file)
 
 def _load_creds():
     with open(_creds) as rf:
         return json.load(rf)
 
-def get(key):
-    creds = _load_creds()
-    if key in creds:
-        return decrypt(creds[key])
-
-    return None
-
-def run_cli(args):
-    print(f" \n\n\tJOLENE 💈  \n\n \tv{_version}\n")
-    print(f"Current ENV key is {_key().decode()} \n")
-    input(f"Press any key to remove everything and start over. Or CTRL+c to exit\n")
-    print(f" \n\n\tJOLENE SETUP 💈 \n\n")
-    print(f"By continuing {_creds} will be obliterated and the current JOLENE key will be rendered useless. \n ")
-    input("Ok?")
-
-    input("Generate new JOLENE key [enter] \n")
-    new_key = _gen_key()
-    print(f"New key is {new_key} \n")
-
+def user_input_data():
     print(f"Enter data to ecode in JSON format with JOLENE. \n eg. username: jolene, password: i love jolene, date: 7/10/2017")
     entering = True
 
@@ -71,16 +57,51 @@ def run_cli(args):
         key, value = row.split(": ")
         data[key] = value
 
+    return data
+
+def dic_diff(og, edit):
+    for key, value in edit.items():
+        og[key] = value
+    return og
+
+def add_data(dic):
+    new = encrypt_dic(dic)
+    write(dic_diff(_load_creds(), new))
+
+def get(key):
+    creds = _load_creds()
+    if key in creds:
+        return decrypt(creds[key])
+
+    return None
+
+def run_cli(args):
+    print(f" \n\n\tJOLENE 💈  \n\n \tv{_version}\n")
+    if _key() is not None: 
+        print(f"Current ENV key is {_key().decode()} \n")
+        inp = input(f"Add data to JOLENE? (y/n): ")
+        if inp == "y":
+            add_data(user_input_data())
+            return
+
+
+    inp = input(f"Remove everything and start over (y/n): ")
+    if (inp != "y"): return
+    print(f" \n\n\tJOLENE SETUP 💈 \n\n")
+    print(f"By continuing {_creds} will be obliterated and the current JOLENE key will be rendered useless. \n ")
+    input("Ok?")
+
+    input("Generate new JOLENE key [enter] \n")
+    new_key = _gen_key()
+    print(f"New key is {new_key} \n")
+
+    data = user_input_data() 
+
     enc = encrypt_dic(data)
     print(enc)
-
-    with open(_creds, "w") as write_file:
-        json.dump(enc, write_file)
+    write(enc)
 
     print(f"Exported encrypted data to {_creds} \n")
     print(f"Please run $\t {_set_key(new_key)}")
 
 
-
-if len(sys.argv) > 1:
-    run_cli(sys.argv)
